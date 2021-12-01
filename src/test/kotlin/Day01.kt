53080733d4962e8086dea4efe3e0be8b684a67d7
Norbert Kiesel <nkiesel@metricstream.com>
2021-11-30 21:27:26 -0800

    using the more simple approach I could think of

---
+++ src/test/kotlin/Day01.kt
@@ -0,0 +1,29 @@
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class Day01 {
+    @Test
+    fun testOne(input: List<String>) {
+        // provide explicit lists for testing other cases than the actual test input
+        one(listOf("199", "200", "208", "210", "200", "207", "240", "269", "260", "263")) shouldBe 7
+        one(input) shouldBe 1288
+    }
+
+    @Test
+    fun testTwo(input: List<String>) {
+        two(listOf("607", "618", "618", "617", "647", "716", "769", "792")) shouldBe 5
+        two(input) shouldBe 1311
+    }
+
+    private fun one(input: List<String>): Int = input.asSequence()
+        .map(String::toInt)
+        .zipWithNext()
+        .count { (a, b) -> b > a }
+
+    private fun two(input: List<String>): Int = input.asSequence()
+        .map(String::toInt)
+        .windowed(3)
+        .map { it.sum() }
+        .zipWithNext()
+        .count { (a, b) -> b > a }
+}

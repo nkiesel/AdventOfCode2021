63f0b0c132c7774e83e6b37a56b5c91206c2c8c6
Norbert Kiesel <nkiesel@metricstream.com>
2021-12-08 22:22:17 -0800

    Day09

---
+++ src/test/kotlin/Day09.kt
@@ -0,0 +1,82 @@
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class Day09 {
+    private val sample = """
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
+""".trimIndent()
+
+    @Test
+    fun testOne(input: List<String>) {
+        one(sample.lines()) shouldBe 15
+        one(input) shouldBe 562
+    }
+
+    @Test
+    fun testTwo(input: List<String>) {
+        two(sample.lines()) shouldBe 1134
+        two(input) shouldBe 1076922
+    }
+
+    private fun one(input: List<String>): Int {
+        val map = input.map { line -> line.toList().map { it.digitToInt() }.toIntArray() }.toTypedArray()
+        var lowPoints = 0
+        for (x in map[0].indices) {
+            for (y in map.indices) {
+                val p = map[y][x]
+                if (p != 9 && map.lowPoint(x, y)) {
+                    lowPoints += p + 1
+                }
+            }
+        }
+        return lowPoints
+    }
+
+    private fun Array<IntArray>.lowPoint(x: Int, y: Int): Boolean {
+        val p = this[y][x]
+        for ((dx, dy) in listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1)) {
+            val cx = x + dx
+            val cy = y + dy
+            if (cx in this[0].indices && cy in this.indices) {
+                if (this[cy][cx] <= p) return false
+            }
+        }
+        return true
+    }
+
+    private fun two(input: List<String>): Int {
+        val map = input.map { line -> line.toList().map { it.digitToInt() }.toIntArray() }.toTypedArray()
+        return buildList {
+            for (x in map[0].indices) {
+                for (y in map.indices) {
+                    if (map[y][x] != 9) {
+                        add(map.basinSize(x, y))
+                    }
+                }
+            }
+        }.sortedDescending().take(3).reduce { product, i -> product * i }
+    }
+
+    private fun Array<IntArray>.basinSize(x: Int, y: Int): Int {
+        var size = 1
+        this[y][x] = 9
+        for ((dx, dy) in listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1)) {
+            val cx = x + dx
+            val cy = y + dy
+            if (cx in this[0].indices && cy in this.indices) {
+                if (this[cy][cx] != 9) size += basinSize(cx, cy)
+            }
+        }
+        return size
+    }
+}
+
+// This was pretty straight-forward: walk over the map and look at surroundings. For part 2,
+// I set cells to 9 while measuring a basin to avoid looking at the same cell more than once.
+// I only had one hick-up: computation of the neighbors.  I initially iterated over -1..1 for
+// dx and dy and missed excluding the center.  I then simply explicitly listed the 4 pairs of
+// offsets.

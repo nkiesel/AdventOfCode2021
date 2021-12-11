e18ed1fc6dbb39e6b33fff563ea797cb259cdfac
Norbert Kiesel <nkiesel@metricstream.com>
2021-12-11 12:20:54 -0800

    Day11 slight improvement

--- src/test/kotlin/Day11.kt
+++ src/test/kotlin/Day11.kt
@@ -35,37 +35,42 @@ class Day11 {
 
         for (step in 1..maxSteps) {
             // Increment all and collect initial list of flashers
-            var flashers = buildList {
-                for (row in map.indices) for (col in map[row].indices) if (++map[row][col] == 10) add(Pair(row, col))
-            }
+            val flashers = buildList {
+                for (row in map.indices) for (col in map[row].indices) {
+                    if (++map[row][col] == 10) add(Pair(row, col))
+                }
+            }.toMutableList()
 
             // Process all the flashers, collecting new ones along the way
             while (flashers.isNotEmpty()) {
-                flashes += flashers.count()
-                val new = buildList {
-                    for ((row, col) in flashers) {
-                        for ((nr, nc) in map.neighbors(row, col)) {
-                            if (++map[nr][nc] == 10) add(Pair(nr, nc))
-                        }
-                    }
+                for ((nr, nc) in map.neighbors(flashers.removeLast())) {
+                    if (++map[nr][nc] == 10) flashers.add(Pair(nr, nc))
                 }
-                flashers = new
             }
 
             // Reset all flashers to 0.  At this point, values can be > 10
             var allZero = true
-            for (row in map.indices) for (col in map[row].indices) if (map[row][col] >= 10) map[row][col] = 0 else allZero = false
+            for (row in map.indices) for (col in map[row].indices) {
+                if (map[row][col] >= 10) {
+                    map[row][col] = 0
+                    flashes++
+                } else {
+                    allZero = false
+                }
+            }
             if (allZero && firstAllZeros) return Result(step, flashes)
         }
 
         return Result(maxSteps, flashes)
     }
 
-    private fun Array<IntArray>.neighbors(x: Int, y: Int): List<Pair<Int, Int>> =
+    private fun Array<IntArray>.neighbors(p: Pair<Int, Int>): List<Pair<Int, Int>> =
         listOf(-1 to -1, -1 to 0, -1 to 1, 0 to -1, 0 to 1, 1 to -1, 1 to 0, 1 to 1)
-            .map { (dx, dy) -> x + dx to y + dy }
+            .map { (dx, dy) -> p.first + dx to p.second + dy }
             .filter { (cx, cy) -> cx in this[0].indices && cy in this.indices }
-
 }
 
 // Not very elegant, but works. I really should have a proper 2D-map type for these puzzles.
+//
+// Update: I later realized that I do not need a second list; instead I can just use the
+// list as a stack.
